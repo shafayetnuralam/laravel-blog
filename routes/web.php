8853c2d7b6026d2794b955ab852b6f66d143bf04
Shafayet Nur Alam <30376236+shafayetnuralam@users.noreply.github.com>
2022-06-20 21:44:09 +0600

    Routing With Parameter

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\SiteController@Home');
-
-Route::get('/about', 'App\Http\Controllers\SiteController@About');
-
-Route::get('/contact', 'App\Http\Controllers\SiteController@Contact');
+Route::get('/Name/{namevalue}', 'App\Http\Controllers\DemoController@MyName');
